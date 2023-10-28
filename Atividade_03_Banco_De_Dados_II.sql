70b35c16714632160936c9649d09d809bbdeeb59
Adriano <adriano.bezerra4@fatec.sp.gov.br>
2023-10-28 20:55:39 -0300

    Atividade 03 Banco de Dados II - PROCEDURE

--- Atividade_03_Banco_De_Dados_II.sql
+++ Atividade_03_Banco_De_Dados_II.sql
@@ -3,7 +3,7 @@
 '/Uma empresa de vendas tem um banco de dados com informações sobre os seus produtos. Ela precisa criar um relatório que faça um levantamento diário da quantidade de produtos comprados por dia. Para ajudar a empresa, crie um procedure para agilizar esse processo./'
 
 
---Trigger executado
+--Procedure
 
 
 
